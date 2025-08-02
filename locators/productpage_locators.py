eeb1b0899c8ef1d8e71282ace4af4dd8983d503c
Deepak Sumanth <deepak.harish7@gmail.com>
2025-08-02 14:24:18 +0530

    added 1 tc for product

--- locators/productpage_locators.py
+++ locators/productpage_locators.py
@@ -5,6 +5,7 @@ class ProductPageLoc:
     products = (By.CSS_SELECTOR, ".features_items .col-sm-4")
     product_price = (By.CSS_SELECTOR, ".productinfo h2")
     product_name = (By.CSS_SELECTOR, ".productinfo p")
+    view_product = (By.PARTIAL_LINK_TEXT, "product_details")
     #equivalent xpath
     #product_price = (By.XPATH, ".//div[contains(@class, 'productinfo')]//h2")
     #product_name = (By.XPATH, ".//div[contains(@class, 'productinfo')]//p")
